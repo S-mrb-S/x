ea3afcc48c99f9b194bbcfd2d180ddf6c4ec9587
MRB <mehrabshafae@gmail.com>
2024-12-31 12:17:44 +0330

    mrb: core cu

--- src/core.hpp
+++ src/core.hpp
@@ -4,18 +4,18 @@
 #include <stdexcept>
 #include <string>
 #include <cmath>
-#include <memory>
-#include <any>
-#include <future>
+#include <memory> // C++11
+#include <any> // C++17
+#include <future> // C++11
 #include <utility>
 #include <functional>
-#include <chrono>
+#include <chrono> // C++11
 #include <exception>
 #include <sstream>
-#include <taskflow/taskflow.hpp>
-#include <fmt/core.h>
-#include <string_view>
-#include <optional>
+#include <taskflow/taskflow.hpp> // C++11 (depends on library version)
+#include <fmt/core.h> // C++11 (depends on library version)
+#include <string_view> // C++17
+#include <optional> // C++17
 #include <format> // C++20
 
 // local modules
