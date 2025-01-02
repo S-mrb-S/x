d5baaaf3beecd16e6382c4d892c579b3b7b40742
MRB <mehrabshafae@gmail.com>
2025-01-02 10:55:33 +0330

    mrb: Rm tf

--- src/tf.cpp
+++ src/tf.cpp
@@ -1,42 +1,30 @@
-// Base class for task execution
-template<typename ExecutorType>
-class TaskExecutor {
-public:
-    explicit TaskExecutor(size_t thread_count) : executor(thread_count) {}
-
-    TaskExecutor(const TaskExecutor &) = delete;
-    $ &operator=(const TaskExecutor &) = delete;
-
-protected:
-    ExecutorType executor;
-};
 
 // Silent async execution
-class Go : public TaskExecutor<tf::Executor> {
+class Go{
 public:
-    Go() : TaskExecutor(200) {}
+    // Go() : TaskExecutor(200) {}
 
     // Shift left operator for silent async
     template<typename Func>
     $ &operator<<(Func &&func) {
-        executor.silent_async(std::forward<Func>(func));
+        // executor.silent_async(std::forward<Func>(func));
         return *this;
     }
 
     $ waitAll() {
-        executor.wait_for_all();
+        // executor.wait_for_all();
     }
 };
 
 // Async execution with future
-class Async : public TaskExecutor<tf::Executor> {
+class Async{
 public:
-    Async() : TaskExecutor(100) {}
+    // Async() : TaskExecutor(100) {}
 
     // Shift right operator for async
     template<typename Func>
     $ operator>>(Func &&func) {
-        return executor.async(std::forward<Func>(func));
+        // return executor.async(std::forward<Func>(func));
     }
 };
 
