32b09f61fd5c873146b76c99d57ebb921911a251
MRB <mehrabshafae@gmail.com>
2025-01-02 10:55:25 +0330

    mrb: test

--- src/main.cc
+++ src/main.cc
@@ -1,37 +1,41 @@
 #include "core.hpp"
 
+// function hi() {
+//     $ errorCode = 404;
+//     $ errorMessage = "Not Found";
+//
+//     console.log("Error {}: {}", errorCode, errorMessage);
+//
+//     for (size_t i = 0; i < 30; i++) {
+//         go << lm {
+//             // task
+//             // go << lm {
+//             //     // task
+//             //     // std::this_thread::sleep_for(std::chrono::seconds(1));
+//             // };
+//         };
+//     }
+//
+//     go.waitAll();
+//
+//     $ result1 = async >> lm {
+//         console.log("Task 1");
+//         return 1;
+//     };
+//
+//     $ result2 = async >> lm {
+//         console.log("Task 2");
+//         return 2;
+//     };
+//
+//     result2.wait();
+//
+//     console.log("Result 1: {}", result1.get());
+//     console.log("Result 2: {}", result2.get());
+// }
+
 function hi() {
-    $ errorCode = 404;
-    $ errorMessage = "Not Found";
 
-    console.log("Error {}: {}", errorCode, errorMessage);
-
-    for (size_t i = 0; i < 30; i++) {
-        go << lm {
-            // task
-            // go << lm {
-            //     // task
-            //     // std::this_thread::sleep_for(std::chrono::seconds(1));
-            // };
-        };
-    }
-
-    go.waitAll();
-
-    $ result1 = async >> lm {
-        console.log("Task 1");
-        return 1;
-    };
-
-    $ result2 = async >> lm {
-        console.log("Task 2");
-        return 2;
-    };
-
-    result2.wait();
-
-    console.log("Result 1: {}", result1.get());
-    console.log("Result 2: {}", result2.get());
 }
 
 int main() {
