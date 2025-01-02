947f8fd8dbd509a35c2692d63c819cd0c44d8178
MRB <mehrabshafae@gmail.com>
2025-01-02 10:55:18 +0330

    mrb: Remove TaskFlow

--- src/core.hpp
+++ src/core.hpp
@@ -12,10 +12,13 @@
 #include <chrono> // C++11
 #include <exception>
 #include <sstream>
-#include <taskflow/taskflow.hpp> // C++11 (depends on library version)
+#include <omp.h> // OpenMp
 #include <fmt/core.h> // C++11 (depends on library version)
 #include <string_view> // C++17
 #include <optional> // C++17
+#include <vector>
+#include <atomic>
+#include <unistd.h>
 
 // local modules
 #include "core.cpp"
