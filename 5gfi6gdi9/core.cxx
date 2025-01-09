baa4b80f8a3991bc08e03d809e5bf555c652f2c0
MRB <mehrabshafae@gmail.com>
2025-01-09 15:46:25 +0330

    mv to 5gfi6gdi9 folder

--- 5gfi6gdi9/core.cxx
+++ 5gfi6gdi9/core.cxx
@@ -1,5 +1,48 @@
 #pragma once
-#include "core.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <cmath>
+#include <memory> // C++11
+#include <any> // C++17
+#include <future> // C++11
+#include <utility>
+#include <functional>
+#include <chrono> // C++11
+#include <exception>
+#include <sstream>
+#include <format>
+#include <mutex>
+
+#ifdef USE_FMT
+#include <fmt/core.h>
+#endif
+
+#ifdef USE_OPENMP
+#include <omp.h>
+#endif
+
+#include <curl/curl.h>
+
+#include <string_view> // C++17
+#include <optional> // C++17
+#include <vector>
+#include <atomic>
+#include <unistd.h>
+
+// auto macros
+
+#define function inline __attribute__((always_inline)) auto
+#define $ auto
+
+#define elif else if
+#define echo(x) std::cout << x << std::endl // printing to console
+#define echo_err(x) std::cerr << x << std::endl // print error to console
+
+#define lm []()
+#define lma [&]()
+
 
 template <typename... Args>
 function rformat(const std::string_view content, Args &&...args)
--- 5gfi6gdi9/main.cc
+++ 5gfi6gdi9/main.cc
@@ -1,4 +1,4 @@
-#include "core.hpp"
+#include "core.cxx"
 
 function hi() {
     // $ errorCode = 404;
