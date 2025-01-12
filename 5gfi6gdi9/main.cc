f27d454736ea73744e07405430b4f1d2834e9450
MRB <mehrabshafae@gmail.com>
2025-01-12 12:16:00 +0330

    cleanup

--- 5gfi6gdi9/main.cc
+++ 5gfi6gdi9/main.cc
@@ -3,16 +3,16 @@
 #define $run int main() {run(); return 0;}
 
 $ run() {
-    try {
-
-    } catch (const std::runtime_error &e) {
-        std::cerr << "Runtime error: " << e.what() << std::endl;
-        return 0;
-    }
-    catch (...) {
-        std::cerr << "An unknown error occurred." << std::endl;
-        return 0;
-    }
+    // try {
+    //
+    // } catch (const std::runtime_error &e) {
+    //     std::cerr << "Runtime error: " << e.what() << std::endl;
+    //     return 0;
+    // }
+    // catch (...) {
+    //     std::cerr << "An unknown error occurred." << std::endl;
+    //     return 0;
+    // }
 }
 
 $run
